3de53c02e685cbe7124c3e87c144959ade624cc7
Amitosh <amitosh.verma9@gmail.com>
2023-07-26 22:12:57 +0530

    String and Stringbuilder methods and the difference
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/AllAbooutStrings/main.java
+++ src/main/java/AllAbooutStrings/main.java
@@ -1,2 +1,36 @@
-package AllAbooutStrings;public class main {
+package AllAbooutStrings;
+
+public class main {
+    public static void main(String[] args) {
+        String birthDate = "25/11/1999";
+        int startingIndex = birthDate.indexOf("1999");
+        System.out.println("Starting index of birthdate is " + startingIndex);
+        System.out.println("birth year is " + birthDate.substring(startingIndex));
+
+        String newDate = String.join("/", "09", "09", "1998");
+        System.out.println(newDate);
+
+        newDate = "23";
+        newDate = newDate.concat("/");
+        newDate = newDate.concat("04");
+        newDate = newDate.concat("/");
+        newDate = newDate.concat("1999");
+        System.out.println(newDate);
+        System.out.println(newDate.replace('/', '-'));
+
+
+
+
+        String str = "Hello World";
+        StringBuilder stringBuilder = new StringBuilder("Hello World");
+        System.out.println(str);
+        System.out.println(stringBuilder);
+
+        str.concat("and GoodBye");
+        stringBuilder.append("and GoodBye");
+        System.out.println(str);
+        System.out.println(stringBuilder);
+
+
+    }
 }
