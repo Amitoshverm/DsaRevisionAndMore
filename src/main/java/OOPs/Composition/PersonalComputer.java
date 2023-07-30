bca4817b7fb76c5b21d4ba364128a718d091ac01
Amitosh <amitosh.verma9@gmail.com>
2023-07-30 15:46:26 +0530

    Compositon on glance
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/OOPs/Composition/PersonalComputer.java
+++ src/main/java/OOPs/Composition/PersonalComputer.java
@@ -1,2 +1,27 @@
-package OOPs.Composition;public class PersonalComputer {
+package OOPs.Composition;
+
+public class PersonalComputer extends Product{
+
+    private Monitor monitor;
+    private ComputerCase computerCase;
+    private MotherBoard motherBoard;
+
+    public PersonalComputer(String model, String manufacturer, Monitor monitor, ComputerCase computerCase, MotherBoard motherBoard) {
+        super(model, manufacturer);
+        this.monitor = monitor;
+        this.computerCase = computerCase;
+        this.motherBoard = motherBoard;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
+
+    public MotherBoard getMotherBoard() {
+        return motherBoard;
+    }
 }
--- src/main/java/OOPs/Composition/main.java
+++ src/main/java/OOPs/Composition/main.java
@@ -1,2 +1,16 @@
-package OOPs.Composition;public class main {
+package OOPs.Composition;
+
+public class main {
+    public static void main(String[] args) {
+        ComputerCase computerCase = new ComputerCase("2800", "dell", "240");
+        Monitor monitor = new Monitor("27 inch", "Acer", 27, "2540 x 1750");
+        MotherBoard motherBoard = new MotherBoard("BJ-200", "Asus", 6, 8, "v2.20");
+
+        PersonalComputer pc = new PersonalComputer("2022", "dell", monitor, computerCase, motherBoard);
+        pc.getMonitor().drawPixelAt(12, 15, "blue");
+        pc.getMotherBoard().loadProgram("Fibonacci");
+        pc.getComputerCase().pressPowerSupplyButton();
+
+
+    }
 }
