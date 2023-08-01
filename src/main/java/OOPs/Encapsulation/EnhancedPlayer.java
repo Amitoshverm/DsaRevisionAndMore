39b6ad31cd686fedc80a94d4f914070aafab8cfe
Amitosh <amitosh.verma9@gmail.com>
2023-08-01 16:50:16 +0530

    how this class is encapsulated and diff from Player one
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/OOPs/Encapsulation/EnhancedPlayer.java
+++ src/main/java/OOPs/Encapsulation/EnhancedPlayer.java
@@ -1,7 +1,19 @@
 package OOPs.Encapsulation;
 
 public class EnhancedPlayer {
+
+    // here fields are encapsulated as private and will be passed as arguments in constructor
     private String playerName;
     private int health;
     private String weapon;
+
+    public EnhancedPlayer(String playerName) {
+        this(playerName, 100, "sword");
+    }
+
+    public EnhancedPlayer(String playerName, int health, String weapon) {
+        this.playerName = playerName;
+        this.health = health;
+        this.weapon = weapon;
+    }
 }
