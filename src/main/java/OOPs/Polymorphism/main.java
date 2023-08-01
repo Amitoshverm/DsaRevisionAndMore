c8ea1416076a640f432721af321588e4c98f8284
Amitosh <amitosh.verma9@gmail.com>
2023-08-01 23:30:46 +0530

    good way of providing input using factory method
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/OOPs/Polymorphism/main.java
+++ src/main/java/OOPs/Polymorphism/main.java
@@ -1,8 +1,33 @@
 package OOPs.Polymorphism;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-       Movie movie = Movie.getMovie("Adventure", "Star Wars");
-       movie.watchMovie();
+//       Movie movie1 = Movie.getMovie("Adventure", "Star Wars");
+//       movie1.watchMovie();
+//
+//       Movie movie2 = Movie.getMovie("Scifi", "Interstellar");
+//       movie2.watchMovie();
+//
+//       Movie movie3 = Movie.getMovie("Comedy", "21 jump street");
+//       movie3.watchMovie();
+        Scanner in = new Scanner(System.in);
+//        String type = in.nextLine();
+//        String title = in.nextLine();
+//        Movie movie = Movie.getMovie(type, title);
+//        movie.watchMovie();
+        while (true) {
+            System.out.print("Enter the following for the genre- A for adventure, C for comedy, S for scifi and Q for quit : ");
+            String type = in.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+            System.out.print("enter Movie title: ");
+            String title = in.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
     }
 }
