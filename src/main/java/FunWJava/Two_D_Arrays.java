e6208cb87a1f1d948ddb94b5ab8b312f57af77c5
Amitosh <amitosh.verma9@gmail.com>
2023-08-21 20:19:35 +0530

    diagonal right to left
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/FunWJava/Two_D_Arrays.java
+++ src/main/java/FunWJava/Two_D_Arrays.java
@@ -2,17 +2,18 @@ package FunWJava;
 
 public class Two_D_Arrays {
     public static void main(String[] args) {
-        int[][] arr = {{2, 1, 3},
-                      {4, 2, 6},
-                      {9, 10 ,8}};
+        int[][] arr = {{2, 1, 3, 9},
+                      {4, 2, 6, 22},
+                      {9, 10 ,8, 11},
+                      {2, 4, 6, 12}};
 
-        int i = 0, j = arr.length-1;
-        while (i < arr.length) {
-            if (i + j == arr.length-1) {
-                System.out.println(arr[i][j]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr.length-1; j >= 0; j--) {
+                if (i + j == arr.length-1) {
+                    System.out.print(arr[i][j]+ " ");
+                }
             }
-            i++;
-            j--;
+
         }
     }
 }
