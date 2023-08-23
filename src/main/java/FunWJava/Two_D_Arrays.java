645ce1cf069b3485852a1dc3acad63a3988b9928
Amitosh <amitosh.verma9@gmail.com>
2023-08-23 22:06:18 +0530

    added the class
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/FunWJava/Two_D_Arrays.java
+++ src/main/java/FunWJava/Two_D_Arrays.java
@@ -1,15 +1,23 @@
 package FunWJava;
 
+import java.util.Arrays;
+
 public class Two_D_Arrays {
     public static void main(String[] args) {
-        int[][] arr = {{2, 1, 3, 9},
-                      {4, 2, 6, 22},
-                      {9, 10 ,8, 11},
-                      {2, 4, 6, 12}};
-
+        int[][] arr ={{1, 2, 4},
+                      {6, 1, 3},
+                      {11, 3, 2}};
+        int[][] A = new int[arr.length][arr.length];
         for (int i = 0; i < arr.length; i++) {
-            int j = arr.length - 1 - i;
-                    System.out.print(arr[i][j]+ " ");
+            for (int j = 0; j < arr.length; j++) {
+                A[i][j] = arr[j][i];
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                System.out.print(A[i][j] +" ");
+            }
+            System.out.println();
         }
     }
 }
