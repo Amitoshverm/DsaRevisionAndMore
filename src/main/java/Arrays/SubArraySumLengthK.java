ef3325d38e6f5b4848aad42d5b9e1be4e45aa468
Amitosh <amitosh.verma9@gmail.com>
2024-02-22 14:05:06 +0530

    done with tictactoe and one dsa problem
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/Arrays/SubArraySumLengthK.java
+++ src/main/java/Arrays/SubArraySumLengthK.java
@@ -1,2 +1,25 @@
-package Arrays;public class SubArraySumLengthK {
+package Arrays;
+
+import java.util.HashMap;
+
+public class SubArraySumLengthK {
+    public static void main(String[] args) {
+        int[] arr ={1, 3, 5 , 5, 1, 3};
+        System.out.println(sol(arr, 2));
+
+
+    }
+    public static int sol(int[] A, int k) {
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += A[i];
+        }
+        int max = sum;
+        for (int i = k; i < A.length; i++) {
+            sum = sum - A[i-k] + A[i];
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
 }
